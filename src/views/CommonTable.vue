ed73bc8e6c611ab9628b3069b9603db647e440cf
nidazhong <nidazhong17@163.com>
2025-11-07 22:59:31 +0800

    feat:列表搜索支持多行条件

--- src/views/CommonTable.vue
+++ src/views/CommonTable.vue
@@ -32,37 +32,6 @@
           </div>
         </el-col>
       </el-row>
-      <!--&lt;!&ndash;5+7+6+6 每个col占据4&ndash;&gt;-->
-      <!--<el-row :gutter="10" >-->
-      <!--  <el-col :span="5" >-->
-      <!--    <el-form-item label="关键字">-->
-      <!--      <el-input placeholder="请输入关键字" />-->
-      <!--    </el-form-item>-->
-      <!--  </el-col>-->
-      <!--  <el-col :span="7" >-->
-      <!--    <el-form-item label="操作时间">-->
-      <!--      <el-date-picker-->
-      <!--          start-placeholder="开始日期"-->
-      <!--          end-placeholder="结束日期"-->
-      <!--          type="daterange"-->
-      <!--          range-separator="至"-->
-      <!--      />-->
-      <!--    </el-form-item>-->
-      <!--  </el-col>-->
-      <!--  &lt;!&ndash;:push=6 表示向右边移动6个栅栏&ndash;&gt;-->
-      <!--  <el-col :span="6" class="btn-col" :push="6">-->
-      <!--    <div class="btn-wrapper">-->
-      <!--      <el-button type="primary" @click="">-->
-      <!--        <el-icon><Search /></el-icon>-->
-      <!--        搜索-->
-      <!--      </el-button>-->
-      <!--      <el-button @click="">-->
-      <!--        <el-icon><Refresh /></el-icon>-->
-      <!--        重置-->
-      <!--      </el-button>-->
-      <!--    </div>-->
-      <!--  </el-col>-->
-      <!--</el-row>-->
     </el-form>
   </div>
   <div class="table-wrapper">
@@ -76,27 +45,26 @@
 
 <style lang="less" scoped>
 
+
+  // 如果row多行 不够高度 仅改这里就行
   @search-wrapper-height:55px; // 可随意变动（有最低高度后，此高度不生效），table区域自适应
 
-
-
   .search-wrapper {
-    min-height: 55px; // 最低高度
     height: @search-wrapper-height;
     border: 1px solid #EBEEF5;
-    //display: flex; // flex会破坏栅格
+    display: flex;
+    align-items: center;
 
 
     // 处理el-row的居中问题
-    // el-form 和 el-row 必须要有高度！否则无法垂直居中 !!!
     .el-form {
-      height: 100%;
+      flex: 1;
       .el-row{
-        //margin: -10px 0 -10px 0;
-        height: 100%;
-        align-items: center; // 内元素垂直居中
+        padding: 5px 0 5px 0;
         .el-col{
-          height: 30px;
+          .el-form-item {
+            margin: 0;
+          }
         }
       }
 
@@ -105,6 +73,7 @@
         display: flex;
         justify-content: flex-end;
         align-items: center;
+
         .btn-wrapper {
           padding-right: 10px;
         }
